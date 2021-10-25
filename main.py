b93a7db6c5126835e210f489ed3e01f59442097d
Lu Qiu <luqiujob@gmail.com>
2021-10-25 16:00:28 -0700

    Finishing iterating the script

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import math
 
 from datetime import datetime
 from functools import partial
-from multiprocessing import Pool, log_to_stderr
+from multiprocessing import Pool
 
 import torch
 import torch.nn.parallel
@@ -29,24 +29,25 @@ def parse():
                          if name.islower() and not name.startswith("__")
                          and callable(models.__dict__[name]))
 
-    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet DALI Data Loading')
     parser.add_argument('data', metavar='DIR', nargs='*',
                         help='path(s) to dataset (if one path is provided, it is assumed\n' +
                              'to have subdirectories named "train" and "val"; alternatively,\n' +
                              'train and val paths can be specified directly by providing both paths as arguments)')
     parser.add_argument('--epochs', default=1, type=int, metavar='N',
-                        help='number of total epochs to run')
+                        help='number of total epochs to run (default: 1)')
     parser.add_argument('--process', default=4, type=int, metavar='N',
-                        help='number of data loading processes (default: 4)')
-    parser.add_argument('-b', '--batch-size', default=256, type=int,
-                        metavar='N', help='mini-batch size per process (default: 256)')
-    parser.add_argument('--print-freq', '-p', default=10, type=int,
-                        metavar='N', help='print frequency (default: 10)')
+                        help='number of data loading processes (default: 4) in each node')
+    parser.add_argument('--batch-size', default=256, type=int, metavar='N',
+                        help='mini-batch size per process (default: 256)')
+    parser.add_argument('--print-freq', default=100, type=int, metavar='N',
+                        help='print frequency (default: 100)')
     args = parser.parse_args()
     return args
 
 
 @pipeline_def
+# create a CPU only dali pipelieg for reading data from the data source
 def create_dali_pipeline(data_dir, num_shards, shard_id):
     images, labels = fn.readers.file(file_root=data_dir,
                                      shard_id=shard_id,
@@ -83,6 +84,51 @@ def create_dali_pipeline(data_dir, num_shards, shard_id):
                                       mirror=mirror)
     return images, labels
 
+def dali(batch_size, train_dir, print_freq, logger, num_shards, shard_id):
+    logger.info('Launching training script in child process: train_dir[{}], batch_size[{}], print_freq[{}], '
+                'num_shards[{}], current_shard_id[{}], starting at[{}]'
+                .format(train_dir, batch_size, print_freq, num_shards, shard_id, datetime.now().time()))
+    start = time.time()
+    pipe = create_dali_pipeline(batch_size=batch_size,
+                                num_threads=1,
+                                seed=12 + shard_id,
+                                data_dir=train_dir,
+                                num_shards=num_shards,
+                                shard_id=shard_id,
+                                device_id=-99999)
+    pipe.build()
+    train_loader = DALIClassificationIterator(pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL)
+
+    # train for one epoch
+    total_size, total_read_time, throughput = train(train_loader, batch_size, print_freq, logger, shard_id)
+    total_time = time.time() - start
+    logger.info('Training in child process ends: data: {} images, total time: {} sec, total read time: {} sec, '
+                'data loading speed: {:3f} img/sec'
+                .format(total_size, total_time, total_read_time, throughput))
+    train_loader.reset()
+    return total_size, total_read_time, throughput, total_time
+
+
+def train(train_loader, batch_size, print_freq, logger, shard_id):
+    train_loader_len = int(math.ceil(train_loader._size / batch_size))
+    start = time.time()
+    end = time.time()
+
+    for i, data in enumerate(train_loader):
+        input = data[0]["data"]
+        target = data[0]["label"].squeeze(-1).long()
+        if i % print_freq == 0:
+            if shard_id == 0:
+                logger.info('[[}/{}] Time: {}'.format(
+                    i, train_loader_len, time.time() - end))
+        # use the sleep to mock actual training logic
+        time.sleep(0.5)
+        end = time.time()
+
+    total_time = time.time() - start
+    throughput = train_loader._size / total_time
+    return train_loader._size, total_time, throughput
+
 
 def main():
     global args
@@ -91,21 +137,13 @@ def main():
     if not len(args.data):
         raise Exception("error: No data set provided")
 
-    args.distributed = False
-    if 'WORLD_SIZE' in os.environ:
-        args.distributed = int(os.environ['WORLD_SIZE']) > 1
-
-    rank = 0
-    if 'RANK' in os.environ:
-        rank = int(os.environ['RANK'])
-
-    args.world_size = 1
-    if args.distributed:
-        print('Inits distributed process group with gloo backend')
-        # Distributed information will be passed in through environment variable WORLD_SIZE and RANK
-        torch.distributed.init_process_group(backend='gloo',
-                                             init_method='env://')
-        args.world_size = torch.distributed.get_world_size()
+    # set cluster logging
+    # TODO(lu) does the log shows twice the info needed?
+    logger = logging.getLogger('dev')
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
 
     if len(args.data) == 1:
         train_dir = os.path.join(args.data[0], 'train')
@@ -119,119 +157,61 @@ def main():
     if 'MASTER_PORT' in os.environ:
         master_port = os.environ['MASTER_PORT']
 
-    # Use multiple processes to mock real machine learning training
+    args.distributed = False
+    if 'WORLD_SIZE' in os.environ:
+        args.distributed = int(os.environ['WORLD_SIZE']) > 1
+
+    # arena doesn't support the pytorch multi-processing
+    # rank is the node rank instead of the process rank
+    rank = 0
+    if 'RANK' in os.environ:
+        rank = int(os.environ['RANK'])
+
+    args.world_size = 1
+    if args.distributed:
+        logger.info('Launching distributed test with gloo backend')
+        # distributed information will be passed in through environment variable WORLD_SIZE and RANK
+        torch.distributed.init_process_group(backend='gloo',
+                                             init_method='env://')
+        args.world_size = torch.distributed.get_world_size()
+        logger.info('Launched distributed test in {} nodes'.format(args.world_size))
+
+    # mock the actual machine learning process
+    # each process in each node read a portion of the whole dataset
     num_shards = args.world_size * args.process
     shard_id = range(rank * args.process, (rank + 1) * args.process)
 
-    print('Launching training script: train_dir[{}], world_size[{}], rank[{}], batch_size[{}], processes[{}], '
-          'num_shards[{}], current_shard_id[{}], master_addr[{}], master_port[{}]'
-          .format(train_dir, args.world_size, rank, args.batch_size, args.process, num_shards,
-                  shard_id, master_addr, master_port))
-    logger = logging.getLogger('dev')
-    logger.setLevel(logging.INFO)
+    logger.info('Launching training script: train_dir[{}], world_size[{}], master_addr[{}], master_port[{}], '
+                'batch_size[{}], rank[{}], processes[{}], num_shards[{}], current_shard_id[{}]'
+                .format(train_dir, args.world_size, master_addr, master_port, args.batch_size,
+                        rank, args.process, num_shards, shard_id))
 
-    fileHandler = logging.FileHandler('test.log')
-    fileHandler.setLevel(logging.INFO)
-
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.INFO)
-
-    logger.addHandler(fileHandler)
-    logger.addHandler(consoleHandler)
-
-    logger.info('information message')
     pool = Pool(processes=args.process)
-    dali_func = partial(dali, args.batch_size, train_dir, args.print_freq, num_shards)
+    dali_func = partial(dali, args.batch_size, train_dir, args.print_freq, logger, num_shards)
 
     for epoch in range(0, args.epochs):
         results = pool.map(dali_func, shard_id)
         total_size = 0.0
-        total_time = 0.0
+        total_read_time = 0.0
         image_per_second = 0.0
+        total_time = 0.0
         for result in results:
             total_size += result[0]
-            total_time += result[1]
+            total_read_time += result[1]
             image_per_second += result[2]
+            total_time += result[3]
         # TODO(lu) add a socket to receive the img/sec from all nodes in the cluster
-        logger.info(
-            "Epoch {} Training end: Average speed: {:3f} img/sec, Total time: {:3f} sec, Total size: {:3f} images"
-            .format(epoch, image_per_second, total_time, total_size))
-        logger.info("Sleeping, time to clear system buffer cache")
+        logger.info("Epoch {} training end: process average data: {} images, process average total time: {} sec, "
+                    "process average read time: {} sec, "
+                    "process data loading speed: {:3f} img/sec, process number: {}, "
+                    "node data loading speed: {:3f} img/sec"
+                    .format(epoch, total_size / args.process, total_time / args.process, total_read_time / args.process, 
+                            image_per_second / args.process, args.process, image_per_second))
+        # clear buffer cache requires special docker privileges
+        # as a workaround, we clear the buffer cache manually
+        # TODO(lu) enable setting docker privileges in arena to support clear buffer cache in script
+        logger.info("Starts sleeping to give time for clearing system buffer cache manually")
         time.sleep(300)
-        logger.info("Running again")
-
-
-def dali(batch_size, train_dir, print_freq, num_shards, shard_id):
-    print('Launching training script in child process: train_dir[{}], batch_size[{}], print_freq[{}], '
-          'num_shards[{}], current_shard_id[{}], starting at[{}]'
-          .format(train_dir, batch_size, print_freq, num_shards, shard_id, datetime.now().time()))
-    pipe = create_dali_pipeline(batch_size=batch_size,
-                                num_threads=1,
-                                seed=12 + shard_id,
-                                data_dir=train_dir,
-                                num_shards=num_shards,
-                                shard_id=shard_id,
-                                device_id=-99999)
-    pipe.build()
-
-    train_loader = DALIClassificationIterator(pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL)
-
-    # train for one epoch
-    total_size, total_duration, throughput = train(train_loader, batch_size, print_freq, shard_id)
-    train_loader.reset()
-    return total_size, total_duration, throughput
-
-
-def train(train_loader, batch_size, print_freq, shard_id):
-    batch_time = AverageMeter()
-    train_loader_len = int(math.ceil(train_loader._size / batch_size))
-    start = time.time()
-    end = time.time()
-
-    count = 0
-    for i, data in enumerate(train_loader):
-        count = count + 1
-        input = data[0]["data"]
-        target = data[0]["label"].squeeze(-1).long()
-        if i % print_freq == 0:
-            batch_time.update((time.time() - end) / args.print_freq)
-            end = time.time()
-
-            if shard_id == 0:
-                print('[[{0}/{1}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Speed {2:.3f} ({3:.3f})\t'.format(
-                    i, train_loader_len,
-                    args.world_size * args.batch_size / batch_time.val,
-                    args.world_size * args.batch_size / batch_time.avg,
-                    batch_time=batch_time))
-        # Use the time.sleep to replace the actual training logics
-        time.sleep(0.5)
-
-    total_time = time.time() - start
-    throughput = train_loader._size / total_time
-    print('Training in child process ends: Speed: {} image/s, Data Size: {} images, End Time: {}'
-          .format(throughput, train_loader._size, datetime.now().time()))
-    return train_loader._size, total_time, throughput
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
 
 
 if __name__ == '__main__':
